51b688c364226e50a2c6957e71d95b0361c5405b
Mikhail Maslo <maslomihail@gmail.com>
2018-04-21 12:11:14 +0300

    intial

---
+++ SmartTips/configuration/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  SmartTips
+//
+//  Created by Mikhail Maslo on 21.04.2018.
+//  Copyright © 2018 Mikhail Maslo. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
+
+extension AppDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
